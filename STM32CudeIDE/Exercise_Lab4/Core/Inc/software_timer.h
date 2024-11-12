db2fd8cd5081e7cd6edd0e77c5654f433e75dcf7
NQK-2001 <kiet166810@gmail.com>
2024-11-12 12:34:35 +0700

    update

---
+++ STM32CudeIDE/Exercise_Lab4/Core/Inc/software_timer.h
@@ -0,0 +1,21 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 9, 2024
+ *      Author: Nguyen Quoc Kiet
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+#include "main.h"
+#include "scheduler.h"
+#include "button.h"
+
+extern int timer1_flag;
+extern int timer2_flag;
+
+void timerRun();
+
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
